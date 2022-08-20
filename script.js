11dd1c1c088c11c3f2f4bf32cbd8eda66cff9736
Yasin Çoban <yasincoban1998@outlook.com>
2022-08-21 00:56:20 +0300

    keydown-event_complete and wrongletters update and incoming

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const word_el = document.getElementById("word");
 const popup = document.getElementById("popup-container");
 const message_el = document.getElementById("success-message");
+const wrongLetters_el = document.getElementById("wrong-letters");
 
 function getRandomWord() {
   const words = ["JAVASCRIPT", "JAVA", "PYTHON"];
   return words[Math.floor(Math.random() * words.length)];
 }
 
-const correctLetters = ["J", "A"];
+const correctLetters = [];
 const wrongLetter = [];
+const selectedWord = getRandomWord();
 
 function displayWord() {
-  const selectedWord = getRandomWord();
-
   word_el.innerHTML = `${selectedWord
     .split("")
     .map(
@@ -25,15 +25,36 @@ function displayWord() {
 
   const w = word_el.innerText.replace(/\n/g, "");
   if (w == selectedWord) {
-    popup.style.display = flex;
+    popup.style.display = "flex";
     message_el.innerText = "Tebrikler. Kazandınız";
   }
 }
 
 window.addEventListener("keydown", function (e) {
   if (e.keyCode >= 65 && e.keyCode <= 90) {
-
+    const letter = e.key;
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        //BU HARFI KULLANDINIZ
+      }
+    } else {
+      if (!wrongLetter.includes(letter)) {
+        wrongLetter.push(letter);
+       updateWrongLetters();
+      }
+    }
   }
 });
+function updateWrongLetters(){
+  wrongLetters_el.innerHTML =`
+  
+  ${wrongLetter.length >0 ? '<h3>Hatalı Harfler</h3>':''}
+  ${wrongLetter.map(letter=>`<span>${letter}</span>`)}
+  `;
 
+  
+}
 displayWord();
